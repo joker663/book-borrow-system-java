c330978a8791290e75357196d3dbc436f04bd799
lizhihao <2564047684@qq.com>
2024-05-01 18:36:15 +0800

    feat：新增echarts数据接口接口

---
+++ src/main/java/com/lzh/controller/EchartsController.java
@@ -0,0 +1,16 @@
+package com.lzh.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: lzh
+ * @Date: 2024-02-19
+ */
+@Api(tags = "echarts数据接口")
+@RestController
+@RequestMapping("/echarts")
+public class EchartsController {
+}
