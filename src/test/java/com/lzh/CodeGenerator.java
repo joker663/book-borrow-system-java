cb60807b4461e3f5a9244626f001300fe22da1a9
joker663 <76237373+joker663@users.noreply.github.com>
2024-05-25 19:19:31 +0800

    Update CodeGenerator

--- src/test/java/com/lzh/CodeGenerator.java
+++ src/test/java/com/lzh/CodeGenerator.java
@@ -6,11 +6,6 @@ import com.baomidou.mybatisplus.generator.config.OutputFile;
 
 import java.util.Collections;
 
-/**
- * mp代码生成器
- * by 青哥哥
- * @since 2022-01-26
- */
 public class CodeGenerator {
 
     public static void main(String[] args) {
